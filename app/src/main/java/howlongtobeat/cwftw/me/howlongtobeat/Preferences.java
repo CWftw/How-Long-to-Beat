078ee373e8dee513ad324f15224d827d715f0798
Matt Allen <videogames_rule12@hotmail.com>
2015-12-06 22:14:52 -0500

    Changed preferences

--- app/src/main/java/howlongtobeat/cwftw/me/howlongtobeat/Preferences.java
+++ app/src/main/java/howlongtobeat/cwftw/me/howlongtobeat/Preferences.java
@@ -3,30 +3,22 @@ package howlongtobeat.cwftw.me.howlongtobeat;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 /**
- * A simple {@link Fragment} subclass.
+ * A simple {@link PreferenceFragment} subclass.
  * Activities that contain this fragment must implement the
  * {@link Preferences.OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link Preferences#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Preferences extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
+public class Preferences extends PreferenceFragment
+{
     private OnFragmentInteractionListener mListener;
 
     /**
@@ -37,56 +29,60 @@ public class Preferences extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment Preferences.
      */
-    // TODO: Rename and change types and number of parameters
-    public static Preferences newInstance(String param1, String param2) {
+    public static Preferences newInstance(String param1, String param2)
+    {
         Preferences fragment = new Preferences();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
-    public Preferences() {
+    public Preferences()
+    {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        addPreferencesFromResource(R.xml.preferences);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_preferences, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
+    public void onButtonPressed(Uri uri)
+    {
+        if (mListener != null)
+        {
             mListener.onFragmentInteraction(uri);
         }
     }
 
     @Override
-    public void onAttach(Activity activity) {
+    public void onAttach(Activity activity)
+    {
         super.onAttach(activity);
-        try {
+        try
+        {
             mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
+        }
+        catch (ClassCastException e)
+        {
             throw new ClassCastException(activity.toString()
                     + " must implement OnFragmentInteractionListener");
         }
     }
 
     @Override
-    public void onDetach() {
+    public void onDetach()
+    {
         super.onDetach();
         mListener = null;
     }
@@ -101,9 +97,8 @@ public class Preferences extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
+    public interface OnFragmentInteractionListener
+    {
         public void onFragmentInteraction(Uri uri);
     }
-
 }
